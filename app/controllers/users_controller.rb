0e1ce21c2e7ad64fc90cdebdb4a6877067a43e55
Richard <jullig@acm.org>
2013-06-24 02:25:02 -0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
-  def show
-		@user = User.find(params[:id])
-	end
+   def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
 
   def new
   	@user = User.new
@@ -51,12 +52,6 @@ class UsersController < ApplicationController
 
   private
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
@@ -65,7 +60,7 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
-  end #seems like a spurious end but apparently makes parser happy; perhaps indent is the problem
+  # end seems like a spurious end but apparently makes parser happy; perhaps indent is the problem
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ SampleAppRspec::Application.routes.draw do
   # get "static_pages/contact"
 
   resources :users
-  resources :sessions, only:  [:new, :create, :destroy]
+  resources :sessions,    only: [:new, :create, :destroy]
+  resources :microposts,  only: [:create, :destroy]
 
   root to: 'static_pages#home'
 
